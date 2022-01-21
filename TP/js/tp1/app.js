2eafea9a04c39d14ed36f5c89a40e19cc66e0af5
Kossi-Francois <mondekossi@gmail.com>
2022-01-21 11:08:19 +0100

    ajout js var; js tp1

---
+++ TP/js/tp1/app.js
@@ -0,0 +1,8 @@
+const nbr1 = 2;
+const nbr2 = 3;
+const nbr3 = nbr1; +nbr2;
+
+alert("Bienvenue sur la page de formulaire ");
+
+console.log(nbr1, nbr2);
+console.log("Le résultat est " + nbr3);
---
+++ js/js1Variable/app.js
@@ -0,0 +1,14 @@
+var pays = "France";
+pays = 4;
+pays = true;
+
+if (true) {
+  var nom = "Christian Lisangola";
+  console.log("dans IF : ", nom);
+}
+console.log("En dehors de IF : ", nom);
+
+for (var i = 1; i <= 5; i++) {
+  console.log("i dans la boucle : ", i);
+}
+console.log("i en dehors de la boucle : ", i);
